e68e3335be508af1a81175f09b9280f0548980b2
AbhinandanSengar <abhinandansengar@gmail.com>
2025-05-18 23:35:22 +0530

    Updated models Schema

--- models/Notification.js
+++ models/Notification.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
 
 const notificationSchema = new Schema({
     userId: { type: ObjectId, required: true, ref: "User" },
     type: { type : String, enum: ["email", "sms", "in-app"], required: true },
     message: { type: String, required: true },
     status: { type: String, enum: ["pending" ,"sent", "failed"] , default: "pending" },
+    retryCount: { type: Number, default: 0 },
     createdAt: { type: Date, default: Date.now }
 });
 
--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     phone: { type: String, required: true }
 });
 
